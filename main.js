2b86f7f2a1c8bdf5fded89f734aa4829b0f5a168
Jujin Kim <jk012345@gmail.com>
2018-06-08 00:28:36 +0900

    Added many things Added each page Added some effects

--- main.js
+++ main.js
@@ -31,10 +31,41 @@ function closeWindow() {
 /**
  * Open window
  */
-function openWindow() {
+function openWindow(elem) {
     let window = document.getElementById("main");
     window.classList.remove("close");
     window.classList.add("open");
+
+    //get page url
+    let xhttp = new XMLHttpRequest();
+
+    switch(elem.id) {
+        case 'navBio' :
+            xhttp.open('GET', 'pages/bio.html', false);
+        break;
+        case 'navCareer' :
+            xhttp.open('GET', 'pages/career.html', false);
+        break;
+        case 'navExp' :
+            xhttp.open('GET', 'pages/exp.html', false);
+        break;
+        case 'navPrj' :
+            xhttp.open('GET', 'pages/project.html', false);
+        break;
+        case 'navLink' :
+            xhttp.open('GET', 'pages/link.html', false);
+        break;
+    }
+
+    //push page when loaded
+    xhttp.onreadystatechange = function() {
+        if(xhttp.readyState === 4) {
+            window.innerhtml = xhttp.responseText;
+        }
+    }
+    
+    //load page
+    xhttp.send();
 }
 
 /**
@@ -48,7 +79,7 @@ function closeWindow_OnClick() {
 /**
  * When button on navigation clicked
  */
-function navButton_OnClick() {
-    openWindow();
+function navButton_OnClick(elem) {
+    openWindow(elem);
     shrinkNav();
 }
